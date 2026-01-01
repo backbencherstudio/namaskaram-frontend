1f6045fc1f60b2200300ce657f90680a7182db13
saidul147 <sidul.islam.146@gmail.com>
2026-01-01 11:18:13 +0600

    navbar done

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -4,12 +4,12 @@ import ArrowIcons from '../Icons/ArrowIcons'
 export default function Navbar() {
     return (
         <div className='bg-[#030303] md:py-8 py-6'>
-            <div className="container">
+            <div className="container flex justify-between items-center">
                 <div>
                     <img src="/logo.png" alt="" className='w-[151px] h-[40px]' />
                 </div>
-                <div>
-                    <button>
+                <div className=''>
+                    <button className='bg-[#EFBE84] flex gap-2 md:p-3 p-2 text-[#101010] md:text-base text-sm leading-[150%] font-light'>
                         Buy ASTER
                         <span><ArrowIcons/></span>
                     </button>
