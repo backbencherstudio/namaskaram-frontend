353e58bbfa096c8c04123def6ff234d87f1397eb
saidul147 <sidul.islam.146@gmail.com>
2026-01-01 11:17:56 +0600

    navbar button added

--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ArrowIcons from '../Icons/ArrowIcons'
 
 export default function Navbar() {
     return (
@@ -6,6 +7,12 @@ export default function Navbar() {
             <div className="container">
                 <div>
                     <img src="/logo.png" alt="" className='w-[151px] h-[40px]' />
+                </div>
+                <div>
+                    <button>
+                        Buy ASTER
+                        <span><ArrowIcons/></span>
+                    </button>
                 </div>
             </div>
         </div>
