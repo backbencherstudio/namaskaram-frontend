c8f02ccc45f83db5cd9459c2c03d9c9b94c07833
saidul147 <sidul.islam.146@gmail.com>
2026-01-01 10:51:04 +0600

    git setup

---
+++ app/(Home)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HomeLayout({children,}:Readonly<{children:React.ReactNode}>) {
+  return (
+    <div>
+        <div>
+            
+        </div>
+        {children}
+    </div>
+  )
+}
