8223309c8f6e55e6e74234c6de3e1eb657135df5
saidul147 <sidul.islam.146@gmail.com>
2026-01-01 11:17:17 +0600

    logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import "./globals.css";
 import { AppConfig } from "@/config/app.config";
 import { Toaster } from 'sonner';
 
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["100","200","300","400","500","600","700","800","900"],
+  variable: "--font-outfit",
+});
+
+
 export const metadata: Metadata = {
   title: AppConfig().app.name,
   description: AppConfig().app.slogan,
@@ -15,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}
+      <body className={outfit.className}>{children}
         <Toaster />
       </body>
     </html>
--- components/Shared/Navbar.tsx
+++ components/Shared/Navbar.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='bg-[#030303] md:py-8 py-6'>
+            <div className="container">
+                <div>
+                    <img src="/logo.png" alt="" className='w-[151px] h-[40px]' />
+                </div>
+            </div>
+        </div>
+    )
 }
